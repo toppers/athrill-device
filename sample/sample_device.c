ee17104e3d1d99e9be7d82d41e28f753a4fe8bd0
tmori <tmori@esm.co.jp>
2020-11-22 16:35:30 +0900

    sample is finished

--- sample/sample_device.c
+++ sample/sample_device.c
@@ -1,6 +1,4 @@
-#include "std_errno.h"
-#include "mpu_types.h"
-#include "std_device_ops.h"
+#include "athrill_exdev.h"
 #include <stdlib.h>
 #include <stdio.h>
 
@@ -12,11 +10,13 @@ static Std_ReturnType ex_sampledev_put_data16(MpuAddressRegionType *region, Core
 static Std_ReturnType ex_sampledev_put_data32(MpuAddressRegionType *region, CoreIdType core_id, uint32 addr, uint32 data);
 static Std_ReturnType ex_sampledev_get_pointer(MpuAddressRegionType *region, CoreIdType core_id, uint32 addr, uint8 **data);
 
-
+/**************************************
+ * START: external symbols
+ **************************************/
 int ex_device_memory_size = 2; /* KB */
 char ex_device_memory_data[2048];
 
-MpuAddressRegionOperationType	exdevice_memory_operation = {
+MpuAddressRegionOperationType	ex_device_memory_operation = {
 		.get_data8 		= 	ex_sampledev_get_data8,
 		.get_data16		=	ex_sampledev_get_data16,
 		.get_data32		=	ex_sampledev_get_data32,
@@ -27,19 +27,25 @@ MpuAddressRegionOperationType	exdevice_memory_operation = {
 
 		.get_pointer	= ex_sampledev_get_pointer,
 };
-
-void ex_device_init(MpuAddressRegionType *region)
+static AthrillExDevOperationType *athrill_ex_devop;
+void ex_device_init(AthrillExDevOperationType *athrill_ops)
 {
-	//TODO
+	athrill_ex_devop = athrill_ops;
+	printf("###INFO sample device init success.\n");
 	return;
 }
 
 void ex_device_supply_clock(DeviceClockType *dev_clock)
 {
-	//TODO
+	if (dev_clock->clock == 10000) {
+		printf("OK! sample device supply clock is good. clocks=%lld\n", dev_clock->clock);
+	}
 	return;
 }
 
+/**************************************
+ * END: external symbols
+ **************************************/
 
 static Std_ReturnType ex_sampledev_get_data8(MpuAddressRegionType *region, CoreIdType core_id, uint32 addr, uint8 *data)
 {
