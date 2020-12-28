feccc1f0bc4d2feae3a933517ded264e494e698f
森崇 <tmori@moritakashinoMacBook-Air.local>
2020-12-28 12:53:53 +0900

    lib path changed for server

--- device/sample/grpc/server/ruby/sample_server.rb
+++ device/sample/grpc/server/ruby/sample_server.rb
@@ -1,6 +1,6 @@
 
 this_dir = File.expand_path(File.dirname(__FILE__))
-lib_dir = File.join(this_dir, '../../lib')
+lib_dir = File.join(this_dir, 'lib')
 $LOAD_PATH.unshift(lib_dir) unless $LOAD_PATH.include?(lib_dir)
 
 require 'grpc'
