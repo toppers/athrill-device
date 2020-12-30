28b388ee2db74f6cca666906332df82114d47d98
Takashi Mori <tmori@esm.co.jp>
2020-12-31 08:21:56 +0900

    compile for ubuntu update

--- device/sample/grpc/build/cmake/build.bash
+++ device/sample/grpc/build/cmake/build.bash
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+TARGET_OS=`uname`
 OPENSSL_PATH=/usr/local/opt/openssl
 
 if [ $# -ne 1 ]
@@ -10,12 +11,17 @@ fi
 
 if [ $1 = "all" ]
 then
-	cmake ../.. -DOPENSSL_ROOT_DIR=${OPENSSL_PATH}
+	if [ $TARGET_OS = "Linux" ]
+	then
+		cmake ../.. -DProtobuf_DIR=/usr/local/grpc/lib/cmake/protobuf  -Wno-dev
+	else
+		cmake ../.. -DOPENSSL_ROOT_DIR=${OPENSSL_PATH}
+	fi
 	make
 else
 	rm -rf ./CMakeFiles
 	rm -f Makefile
-	rm -f sample_client*
+	rm -f *sample_client*
 	rm -f sample.*
 	rm -f CMake*
 	rm -f cmake*
