8a133522976455c0687c50fa91d78b4969144e42
takasehideki <takase@i.kyoto-u.ac.jp>
2021-01-04 18:17:00 +0900

    clarifiled the parameter of `clean`

--- device/sample/build/build.bash
+++ device/sample/build/build.bash
@@ -18,13 +18,18 @@ then
 		cmake .. -DOPENSSL_ROOT_DIR=${OPENSSL_PATH}
 	fi
 	make
+elif [ $1 = "clean" ]
+then
+	echo "clean up"
+	rm -r ./CMakeFiles
+	rm Makefile
+	#rm sample_client*
+	#rm sample.*
+	rm CMake*
+	rm cmake*
+	rm libdev*
+	rm -r ./grpc
 else
-	rm -rf ./CMakeFiles
-	rm -f Makefile
-	rm -f sample_client*
-	rm -f sample.*
-	rm -f CMake*
-	rm -f cmake*
-	rm -f libdev*
-	rm -rf ./grpc
+	echo "$0 {all|clean}"
+	exit 1
 fi
