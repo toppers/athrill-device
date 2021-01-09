9e7476c39fde0880197242bf29da049584ac8fde
森崇 <tmori@moritakashinoMacBook-Air.local>
2021-01-09 13:58:10 +0900

    add talker listener

--- device/sample/ros2/run.bash
+++ device/sample/ros2/run.bash
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+ATHRILL_TOP_DIR=$(cd ../../.. && pwd)
 DOCKER_IMAGE=kanetugu2015/ros2-foxy:v1.0.0
 
 sudo docker run \
-	-v `pwd`/workspace:/root/workspace	\
+	-v ${ATHRILL_TOP_DIR}/athrill:/root/workspace/athrill \
+	-v ${ATHRILL_TOP_DIR}/athrill-target-v850e2m:/root/workspace/athrill-target-v850e2m \
+	-v ${ATHRILL_TOP_DIR}/athrill-device:/root/workspace/athrill-device \
+	-v `pwd`/workspace:/root/workspace/ros2	\
 	-it --rm --net host --name ros2-foxy ${DOCKER_IMAGE}
