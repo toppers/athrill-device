0ccb06a0ddb44af5415cd87896061c111a32690f
Takashi Mori <tmori@esm.co.jp>
2021-02-08 18:01:27 +0900

    demo program is chaned

--- device/ev3com/grpc/client/cpp/serial_client_main.cc
+++ device/ev3com/grpc/client/cpp/serial_client_main.cc
@@ -1,12 +1,18 @@
 #include "serial_client.h"
 #include <string.h>
+#include <stdio.h>
 
 int main(int argc, char** argv) 
 {
   char buf[128];
   int retlen;
-  serial_client_init("localhost:50051");
-  serial_client_put_data(1, "Hello World", strlen("Hello World") + 1);
-  serial_client_get_data(1, buf, sizeof(buf), &retlen);
+
+  serial_client_init("172.25.0.1:50051");
+  while (1) {
+    serial_client_get_data(1, buf, sizeof(buf), &retlen);
+    printf("RECV DATA: %s\n",buf);
+    printf("SEND DATA: %s\n",buf);
+    serial_client_put_data(1, buf, strlen(buf));
+  }
   return 0;
 }
